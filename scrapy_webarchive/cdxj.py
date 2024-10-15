e0dea7688f9a469e98c52f38a0c90ab6106290a7
Wesley van Lee <wesley.vanlee@ordina.nl>
2024-10-15 14:29:14 +0200

    Handle file not found for spider and downloader middlewares and improve type hints

--- scrapy_webarchive/cdxj.py
+++ scrapy_webarchive/cdxj.py
@@ -1,10 +1,10 @@
 import json
 import re
-from typing import List
+from dataclasses import dataclass, field
+from typing import Any, List
 
 from cdxj_indexer.main import CDXJIndexer
 
-# based on https://github.com/internetarchive/cdx-summary/blob/main/cdxsummary/parser.py
 CDXREC = re.compile(
     r"^(?P<surt>(?P<host>[^\)\s]+)\)(?P<path>[^\?\s]+)?(\?(?P<query>\S+))?)"
     r"\s(?P<datetime>(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2})(?P<hour>\d{2})(?P<minute>\d{2})(?P<second>\d{2})(?:\d{3})?)"
@@ -12,21 +12,37 @@ CDXREC = re.compile(
 )
 
 
+@dataclass
 class CdxjRecord:
-    def _parse(self, line):
+    wacz_file: Any
+    surt: str
+    host: str
+    path: str = ""
+    query: str = ""
+    datetime: str = ""
+    year: str = ""
+    month: str = ""
+    day: str = ""
+    hour: str = ""
+    minute: str = ""
+    second: str = ""
+    data: dict = field(default_factory=dict)
+
+    @staticmethod
+    def _parse(line: str):
         return CDXREC.match(line)
 
-    def __init__(self, cdxline):
-        m = self._parse(cdxline.strip())
+    @classmethod
+    def from_cdxline(cls, cdxline: str, wacz_file):
+        m = cls._parse(cdxline.strip())
 
         if not m:
             raise ValueError(f"Invalid CDXJ line: '{cdxline.strip()}'")
 
-        for key, value in m.groupdict(default="").items():
-            if key == "data":
-                value = json.loads(value)
+        parsed_data = m.groupdict(default="")
+        parsed_data['data'] = json.loads(parsed_data['data'])
 
-            setattr(self, key, value)
+        return cls(**parsed_data, wacz_file=wacz_file)
 
     def __str__(self):
         return str(self.__dict__)
